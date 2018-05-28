05c0cc0f55bada1066265636a07aa383c6d35447
Your Namemichal0127 <michal.rebis@interia.pl>
2018-05-28 09:40:41 +0200

    python

---
+++ Informatyka podstawa/python/mlotek.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import random
+
+
+def main(args):
+    liczba = random.randint(1, 10)  # losowanie liczby <1; 10>
+    print("Wylosowano:", liczba)
+    # pobranie typu użytkownika
+    odp = input("Podaj liczbę od 1 do 10: ")
+    print("Podana liczba: ", odp)
+
+    if liczba == odp:  # porównywanie z wylosowaną liczbą
+        print("Zgadłeś!")
+    else:
+        print("Spróbuj ponownie")
+
+    return 0
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
