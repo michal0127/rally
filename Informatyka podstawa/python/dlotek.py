012d2cd8974fe21dd7657ac9b90439b45bc1f95c
Your Namemichal0127 <michal.rebis@interia.pl>
2018-06-11 09:42:28 +0200

    dlotek

--- Informatyka podstawa/python/dlotek.py
+++ Informatyka podstawa/python/dlotek.py
@@ -11,13 +11,26 @@ def main(args):
 
     # losowanie liczb
     liczby = []  # lista wylosowanych liczb
+    i = 0  # lista wylosowanych liczb
     # print("Wylosowano:", liczba)
     # pobranie typu użytkownika
-    for i in range(ileliczb):
+    # for i in range(ileliczb):
+    while i < ileliczb:
         liczba = random.randint(1, maksliczba + 1)  # losowanie liczby <1; 10>
         if liczby.count(liczba) == 0:  # sprawdzenie czy wartość jest w liście
             liczby.append(liczba)
+            i += 1  # powiększ i o jeden
     print(liczby)
+
+    typy = set()  # deklaracja pustego zbioru na typy użytkownika
+    i = 0
+    while i < ileliczb:
+            typ = input("Podaj liczbę {}:".format(i + 1))
+            if typ not in typy:  # jeżeli podanego typu nie ma w zbiorze
+                typy.add(typ)
+                i += 1
+    print(typy)
+
     # odp = input("Podaj liczbę od 1 do 10: ")
     # print("Podana liczba: ", odp)
     # if liczba == odp:  # porównywanie z wylosowaną liczbą
