55da0ec392db45ad504655e4a39559c45c24d2fa
michal0127 <michal.rebis@lo1.sandomierz.pl>
2018-10-09 14:18:50 +0200

    python

---
+++ Informatyka/python/liczby-23.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  liczby-23.py
+#  
+
+def liczby2(a, b):
+    """
+    Drukuje wszystkie liczby dwucyfrowe o niepowtarzających się cyfrach
+    oraz zwraca ilość takich liczb, nie drukujemy takich liczb: 11, 22, 33, 
+    44, idt.
+    """
+    for x range(a, b +1, 1)
+        for y in range()
+    
+    pass
+
+def liczby3():
+    """
+    Drukuje wszystkie liczby trzycyfrowe o niepowtarzających się cyfrach
+    oraz zwraca ilość takich liczb, nie drukujemy takich liczb: 113, 212, 373, 
+    441, idt.
+    """
+    
+    pass
+
+def main(args):
+    
+    print(liczba2(a=10, b=99))
+    print(liczby3(a=100, b=999))
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
