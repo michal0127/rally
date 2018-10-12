cad6e2e057921d4f654e24167739667f935b5bb6
michal0127 <michal.rebis@lo1.sandomierz.pl>
2018-10-12 08:44:57 +0200

    kwerendy

--- Webmaster/SQL/dane ucznów/dane_uczniow/uczniowie.sql
+++ Webmaster/SQL/dane ucznów/dane_uczniow/uczniowie.sql
@@ -1,44 +1,44 @@
+DROP TABLE IF EXIST nazwiska;
+CREATE TABLE nazwiska
+(
+    Nr_ucznia INTEGER Primary KEY
+    Nazwisko TEXT(25),
+    Imie1 TEXT(25),
+    Imie2 TEXT(25) DEFAULT NULL,
+):
+
 DROP TABLE IF EXIST dane_osobowe;
 CREATE TABLE dane_osobowe
 (
-    Nr_ucznia INTEGER PRIMARY KEY,
+    Nr_ucznia INTEGER FOREIGN KEY,
     Dzien INTEGER,
     Miesiac INTEGER,
     Rok INTEGER,
     Miejsce_urodzenia TEXT(25),
-    Wojewodztwo TEXT(25)
-);
-
-DROP TABLE IF EXIST nazwiska;
-CREATE TABLE nazwiska
-(
-    Nr_ucznia INTEGER FOREIGN KEY,
-    Nazwisko TEXT(25),
-    Imie1 TEXT(25),
-    Imie2 TEXT(25) DEFAULT NULL,
+    Wojewodztwo TEXT(25),
     FOREIGN KEY (Nr_ucznia) REFERENCES dane_osobowe(Nr_ucznia)
-):
+);
 
 DROP TABLE IF EXIST oceny;
 CREATE TABLE oceny
 (
     Nr_ucznia INTEGER FOREIGN KEY,
-    Zachowanie INTEGER,
-    Religia_Etyka INTEGER,
-    Jpolski INTEGER,
-    Jangielski INTEGER,
-    Jniemiecki INTEGER,
-    Matematyka INTEGER,
-    Hitoria INTEGER,
-    Geografia INTEGER,
-    Biologia INTEGER,
-    Fizyka INTEGER,
-    Chemia INTEGER,
-    Technika INTEGER,
-    Informatyka INTEGER,
-    Plastyka INTEGER,
-    PO INTEGER,
-    WF TEXT,
+    Zachowanie DECIMAL DEFAULT NULL,
+    Religia_Etyka DECIMAL DEFAULT NULL,
+    Jpolski DECIMAL DEFAULT NULL,
+    Jangielski DECIMAL DEFAULT NULL,
+    Jniemiecki DECIMAL DEFAULT NULL,
+    Matematyka DECIMAL DEFAULT NULL,
+    Hitoria DECIMAL DEFAULT NULL,
+    Geografia DECIMAL DEFAULT NULL,
+    Biologia DECIMAL DEFAULT NULL,
+    Fizyka DECIMAL DEFAULT NULL,
+    Chemia DECIMAL DEFAULT NULL,
+    Technika DECIMAL DEFAULT NULL,
+    Informatyka DECIMAL DEFAULT NULL,
+    Plastyka DECIMAL DEFAULT NULL,
+    PO DECIMAL DEFAULT NULL,
+    WF DECIMAL DEFAULT NULL,
     FOREIGN KEY (Nr_ucznia) REFERENCES dane_osobowe(Nr_ucznia)
 );
 );
