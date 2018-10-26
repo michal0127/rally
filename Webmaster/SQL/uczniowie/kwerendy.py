d788ec2f6c31b07900d056c776ce4a89e146bdd9
michal0127 <michal.rebis@lo1.sandomierz.pl>
2018-10-26 08:14:11 +0200

    dalej

--- Webmaster/SQL/uczniowie/kwerendy.py
+++ Webmaster/SQL/uczniowie/kwerendy.py
@@ -6,10 +6,14 @@ import sqlite3
 
 def kwerenda1(cur):
     cur.execute("""
-        SELECT COUNT(*) FROM uczniowie WHERE nazwisko='Piasecki' AND imie='Rafał'
-        """)
+        SELECT klasa, nazwisko, imie, egz_mat FROM klasy INNER JOIN uczniowie ON klasy.id=uczniowie.id_klasa WHERE klasa='1A'
+    """)
         
-        # SELECT 
+        # SELECT nazwisko, imie, egz_hum, egz_mat FROM uczniowie WHERE egz_hum > 40 AND egz_mat > 40 ORDER BY nazwisko ASC 
+        # WHERE plec=1
+        # SELECT imie LIKE '%a'
+        # SELECT nazwisko, imie, egz_hum, egz_mat, FROM uczniowie ORDER BY egz_hum D
+        # SELECT * FROM uczniowie WHERE nazwisko = 'Piasecki'
         # SELECT egz_hum, egz_mat FROM uczniowie WHERE nazwisko='Piasecki' and imie='Rafał'
         # SELECT * FROM uczniowie WHERE nazwisko='Piasecki' AND imie='Rafał'
         # SELECT COUNT(*) FROM uczniowie WHERE nazwisko LIKE 'G%'
