a35cebecc33429811ba40c41e99e8323cf064d02
michal0127 <michal.rebis@interia.pl>
2018-11-05 20:17:21 +0100

    anagram

--- Informatyka/cpp/anagram.cpp
+++ Informatyka/cpp/anagram.cpp
@@ -1,31 +1,49 @@
 /*
  * anagram.cpp
+ * 
+ * Ala ma kota, a kot ma Alę!
+ * alA am atok, a tok am ęlA!
  */
-
+// wyrazy() 
 
 #include <iostream>
 
 using namespace std;
 
-int zlicz(char tb[]) {
+int zlicz(char tb[])
+{
     int i = 0;
+    
     while(tb[i] != '\0') i++;
+    
     return i;
 }
 
-void wyswietl(char tekst[], int rozmiar) {
-    for(int i = 0; i < rozmiar; i++) {
+void wyswietl(char tekst[], int roz)
+{
+    for (int i = 0; i < roz; i++)
+    {
         cout << tekst[i];
     }
 }
 
+void anagram(char tekst[], int roz)
+{
+    for (int i= roz - 1; i >= 0; i--)
+
+    
+}
+
 int main(int argc, char **argv)
 {
-	const int rozmiar = 50;
+    const int rozmiar = 50;
     char tekst[rozmiar];
-    cout << "Podaj słowo: " << endl;
+    
     cin.getline(tekst, rozmiar);
+    
     wyswietl(tekst, zlicz(tekst));
+
+    
 	return 0;
 }
 
