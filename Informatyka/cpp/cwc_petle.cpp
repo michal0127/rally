c095b9e25784f4e9c103a81b8df3ccefb9802d93
michal0127 <michal.rebis@lo1.sandomierz.pl>
2018-11-13 14:19:53 +0100

    ciąg fibonacciego

---
+++ Informatyka/cpp/cwc_petle.cpp
@@ -0,0 +1,33 @@
+/*
+ * cwc_petle.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int fibonacci_it(int n) {
+    int a = 0; // wyraz pierwszy n-2
+    int b = 1; // wyraz drugi n-2
+    if (n < 1) return a;
+    else if (n < 2) return b;
+    int wynik = 0;
+    for (int i = 2; i <=n; i++) {
+        wynik = a + b;
+        a = b;
+        b = wynik;
+    }
+    return wynik;
+}
+
+int main(int argc, char **argv)
+{
+	int n;
+    cout << "Podaj numer wyrazu ciągu: ";
+    cin >> n;
+    cout << "Ciąg Fibonacciego do wyrazu " << n << ":" << endl;
+    cout << fibonacci_it(n);
+	return 0;
+}
+
