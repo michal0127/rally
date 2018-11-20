1ef22a495defbacbfb6411f93f67ba37776a7dd5
michal0127 <michal.rebis@lo1.sandomierz.pl>
2018-11-20 14:21:01 +0100

    rysowanie w pythonie

---
+++ Informatyka/python/rekurencja.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  kwadraty.py
+# 
+
+import turtle
+
+def rysujKwadrat(zolw, bok, ile):
+    # for i in range(4):
+        zolw.forward(bok)
+        zolw.right(190)
+        # if ile > 0:
+        if ile < ile + 1:
+            rysujKwadrat(zolw, bok + 2, ile - 1)
+            
+def rysuj(zolw, bok, kat, warunek):
+    zolw.forward(bok)
+    zolw.right(kat)
+    if kat < warunek:
+        rysuj
+
+
+def main(args):
+    turtle.title("Kwadraty")
+    turtle.setup(1200, 1000)
+   
+    zolw = turtle.Turtle()
+    zolw.color('blue')
+    zolw.speed(0)
+   
+    rysujKwadrat(zolw, 100, 4)
+   
+    turtle.done()
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
+
