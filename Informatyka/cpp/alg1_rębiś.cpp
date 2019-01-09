2a750ceb605e0a9725a940dd18399f425ae8ca7e
michal0127 <michal.rebis@lo1.sandomierz.pl>
2019-01-09 07:34:56 +0100

    Mateusz wajche przełóż

---
+++ Informatyka/cpp/alg1_rębiś.cpp
@@ -0,0 +1,28 @@
+/*
+ * alg1_rębiś.cpp
+ *
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int a = 0;
+    int i = 2;
+    cout << "Podaj liczbę: " << endl;
+    if (a < 0 or a > 100) {
+        }
+    for (int i = 2; i <= 100; i + 2) {
+        if (a == i) {
+            cout << "Parzysta" << endl;
+            }
+    else {
+        cout << "Nieparzysta" << endl;
+        }
+        }
+    
+	return 0;
+}
+
---
+++ Informatyka/python/złożoność/polecenia.txt
@@ -0,0 +1,7 @@
+Napisz implementację pierwszego algorytmu w C++, drugiego w Pythonie.
+Określ, czy podane algorytmy są poprawne – dopisz ocenę w formie
+komentarza.
+
+Nazwa pierwszego pliku: nazwisko_alg1.cpp
+Nazwa drugiego pliku: nazwisko_alg2.py
+
