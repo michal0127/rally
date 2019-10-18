76b403f1aed045bfaf99e6a4ccc637402c24e9fb
michal0127 <michal.rebis@interia.pl>
2019-10-18 14:17:31 +0200

    gsar

--- Informatyka/Bazy danych/uczniowie-98/uczniowie_98.sql
+++ Informatyka/Bazy danych/uczniowie-98/uczniowie_98.sql
@@ -1,40 +1,26 @@
-CREATE TABLE uczniowie
-	id_ucz CHAR(8) UNIQUE
-	imie
-	DROP TABLE IF EXISTS tbUczniowie;
-
-
-CREATE TABLE tbUczniowie
-(
-  id_Ucznia INTEGER PRIMARY KEY AUTOINCREMENT,
-  nazwisko TEXT,
-  imie TEXT
-  ulica TEXT,
-  dom TEXT,
-  id_Klasy INTEGER
-
+DROP TABLE IF EXISTS uczniowie;
+CREATE TABLE uczniowie (
+	id_ucz CHAR(8) PRIMARY KEY,
+	imie VARCHAR(30) NOT NULL CHECK (ime <> ''),
+	nazwisko VARCHAR(30) NOT NULL CHECK (nazwisko <> ''),
+	klasa CHAR (5) DEFAULT ''
 );
 
-DROP TABLE IF EXISTS tbOceny;
-
-CREATE TABLE tbOceny
-(
- id_Ucznia INTEGER NOT NULL,
- ocena INTEGER,
- data DATE,
- id_Przedmiotu INTEGER,
- FOREIGN KEY (id_ucznia) REFERENCES tbUczniowie(id_Ucznia)
-
+DROP TABLE IF EXISTS przedmioty;
+CREATE TABLE przedmioty (
+	id_przedm INTEGER PRIMARY KEY AUTO_INCREMENT,
+	nazwa VARCHAR(60) NOT NULL CHECK (nazwa <> ''),
 );
 
-DROP TABLE IF EXISTS tbPrzedmioty;
-
-CREATE TABLE tbPrzedmioty
-(
- id_Przedmiotu INTEGER NOT NULL,
- nazwa_Przedmiotu TEXT
- nazwisko_Nauczyciela TEXT,
- imie_Naucz TEXT,
- FOREIGN KEY (id_Przedmiotu) REFERENCES tbOceny(id_Przedmiotu)
-
-);
+DROP TABLE IF EXISTS oceny;
+CREATE TABLE (
+    id_oceny INTEGER PRIMARY KEY AUTO_INCREMENT,
+    data DATE NOT NULL,
+    id_ucz CHAR(8),
+    id_przedm INTEGER,
+    ocena DECIMAL(3,2),
+    FOREIGN KEY (id_ucz) REFERENCES uczniowie(id_ucz)
+    ON DELETE CASCADE,
+    FOREIGN KEY (id_przedm)
+    ON DELETE SET NULL
+    );
